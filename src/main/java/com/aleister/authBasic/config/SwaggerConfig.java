40690a9e52eae33fe1ebc52dbffcbb0c0c609fa8
alejandro99billi <billialejandro.salvatierramer01@universitadipavia.it>
2023-07-08 14:39:23 +0200

    fix swagger

--- src/main/java/com/aleister/authBasic/config/SwaggerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.aleister.authBasic.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-
-public class SwaggerConfig {
-
-}
--- src/main/java/com/aleister/authBasic/controller/RegisterController.java
+++ src/main/java/com/aleister/authBasic/controller/RegisterController.java
@@ -3,6 +3,11 @@ package com.aleister.authBasic.controller;
 import com.aleister.authBasic.dto.model.request.RegisterRequest;
 import com.aleister.authBasic.dto.model.response.PersonResponse;
 import com.aleister.authBasic.service.RegisterServiceImp;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.commons.logging.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Tag(name="Register")
 public class RegisterController {
     @Autowired
     private RegisterServiceImp service;
@@ -25,10 +31,14 @@ public class RegisterController {
 
         return (List<PersonResponse>) service.findPersons();
     }
+    //TODO ADD A NEW CLASS FOR MANAGER THE ERROR AND USE THIS ANNOTATION schema=@Schema (implementation = ErrorVM.class)
     @PostMapping("/register/person")
+   @Operation(summary = "Register the user", tags = {"Register"},
+   responses = {@ApiResponse(responseCode = "400",description="Context error",content = @Content(mediaType = "application/json"))})
     private PersonResponse registerPersonWithCredentials (@RequestBody PersonResponse request){
         logger.info("Registrando una nueva persona: {}", request);
         return service.savePerson(request);
 
     }
 }
+
